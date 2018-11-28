624762a4ef874990025abe27058db0b353083a4c
Guillaume.M <macaigne.guillaume@me.com>
2018-11-28 12:40:18 +0100

    Matin1

---
+++ Site/horscourse.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Hors-Course</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <header>
+        <?php include './content/header.html'; ?>
+    </header>
+    <!-- Corps du site -->
+    <main>
+        <!-- Division horizontale -->
+        <section class="container">
+            <!-- Contenu de la division -->
+            <section class="content">
+            <h1>HORS-COURSE</h1>
+            <section class="articles">
+                <article>
+                    <img src="./img/indiscretions.svg" alt="indiscretions">
+                    <p>INDISCRÉTIONS</p>
+                </article>
+                <article>
+                    <p>Lorem ipsum dolor sit amet consectetur...</p>
+                </article>
+                <article>
+                    <p>Lorem ipsum dolor sit amet consectetur...</p>
+                </article>
+                <article>
+                    <p>Lorem ipsum dolor sit amet consectetur...</p>
+                </article>
+            </section>
+            </section>
+        </section>
+    </main>
+    <footer>
+        <?php include '.content/footer.html'; ?>
+    </footer>
+    
+</body>
+</html>
